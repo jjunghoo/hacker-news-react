4a958ae614bdc8a1712c0c9f41080f0a498a534f
Junghoo Park <76745621+jjunghoo@users.noreply.github.com>
2022-02-02 18:10:24 +0900

    feat: Add MainBanner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,44 @@
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import { MainBanner } from "./MainBanner";
+
 export const Home = () => {
-  return "Home";
+  const [bannerAttribute, setBannerAttribute] = useState([
+    {
+      category: "top",
+      text: "Find out most hot issues",
+      background: "rgba(253, 97, 6, 1)"
+    },
+    {
+      category: "new",
+      text: "Fast, Fresh, Fashionable",
+      background: "rgba(123, 97, 255, 1)"
+    },
+    {
+      category: "ask",
+      text: "Ask and get fresh informations",
+      background: "rgba(219, 0, 255, 1)"
+    },
+    {
+      category: "show",
+      text: "Share and grow together",
+      background: "rgba(105, 160, 117, 1)"
+    },
+    {
+      category: "jobs",
+      text: "Your new possibility",
+      background: "rgba(254, 187, 16, 1)"
+    }
+  ]);
+
+  useEffect(() => {});
+
+  console.log(bannerAttribute);
+  return (
+    <div style={{ padding: "10px 20px 0px" }}>
+      {bannerAttribute.map((Attribute) => (
+        <MainBanner key={Attribute.category} attribute={Attribute} />
+      ))}
+    </div>
+  );
 };
---
+++ src/components/MainBanner.jsx
@@ -0,0 +1,50 @@
+import styled from "@emotion/styled";
+
+const MainBannerDiv = styled.div`
+  margin-bottom: 10px;
+  padding: 16px 16.04px 15.04px 19px;
+  /* border: 1px solid black; */
+  border-radius: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* background: rgba(253, 97, 6, 1); */
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
+  .bannerText {
+    color: rgba(255, 255, 255, 1);
+    span {
+      font-size: 20px;
+      font-weight: 700;
+    }
+    p {
+      font-size: 10px;
+      font-weight: 300;
+    }
+  }
+  .more {
+    /* border: 1px solid black; */
+    border-radius: 121px;
+    padding: 9px 20px;
+    background: rgba(255, 255, 255, 1);
+    color: rgba(253, 97, 6, 1);
+    p {
+      font-size: 14px;
+      font-weight: 400;
+    }
+  }
+`;
+
+export const MainBanner = ({ attribute }) => {
+  const { category, text, background } = attribute;
+  return (
+    <MainBannerDiv style={{ backgroundColor: background }}>
+      <div className="bannerText">
+        <span>{category.toUpperCase()} 5</span>
+        <p>{text}</p>
+      </div>
+      <div className="more">
+        <p style={{ color: background }}>More</p>
+      </div>
+    </MainBannerDiv>
+  );
+};
